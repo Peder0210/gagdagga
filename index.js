4a738ac666b3e3ea4596c699e09942e5e5870f4b
LeilaTaran <54849075+LeilaTaran@users.noreply.github.com>
2020-05-04 15:36:24 +0200

    Update index.js
    Koden er reduceret og optimeret ved at slette diverse variable.

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const bcrypt  = require('bcrypt');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const User = require('./models/user');
-const UserLesson = require(`./models/userlesson`)
+const UserLesson = require(`./models/userlesson`);
 const expressSession = require('express-session');
-const logoutController = require("./controllers/logout")
-const Lesson = require("./models/lesson")
+const logoutController = require("./controllers/logout");
+const Lesson = require("./models/lesson");
 const flash = require('connect-flash');
-const cookieParser = require('cookie-parser')
-
-const validator = require(`mongoose-unique-validator`);
+const cookieParser = require('cookie-parser');
 app.use(expressSession({ //Opretter en session.
     secret: 'Temno Player'
 }));
 
 mongoose.connect('mongodb://localhost:27017/wow'), {useNewUrlParser:true};
 var db = mongoose.connection;
+
 app.set('view engine','ejs');
 app.use(express.static('puplic'));
 app.use(bodyParser.json());
@@ -28,50 +27,37 @@ app.use(cookieParser('secret'));
 app.listen(3000,()=>{
    console.log("App listening on port 3000")
     });
-
 app.get('/adminsite', (req,res) =>{
    res.render('adminsite')
 });
-
 app.get('/classSiteUser', (req,res)=>{
     res.render('classSiteUser')
 });
-
-
 app.get('/changelesson', (req,res)=>{
     res.render('changelesson')
 });
 app.get('/classSiteAdmin', ( req,res) => {
     res.render('classSiteAdmin')
 });
-
-
 app.get('/changeuserinfo', (req,res) =>{
     res.render('changeuserinfo')
 });
-
-
-
-
 app.get('/myClasses',  (req,res) =>{
     res.render('myClasses')
 });
-
 app.get('/register', (req,res) =>{
     res.render('register')
 });
-
 app.get('/login', (req,res) =>{
     res.render('Login')
 });
-
 app.get("/logout", logoutController);
 
 app.get('/myPageUser', (req,res) =>{
     res.render('myPageUser')
 });
+//Finder en bruger med de oplyste betingelser og returnerer dataen
 app.post('/registerUser/:userInfo', (req,res) => {
-
     let userInfo = JSON.parse(req.params.userInfo);
  console.log(userInfo);
     User.create(userInfo, (error, result) => {
@@ -81,48 +67,46 @@ app.post('/registerUser/:userInfo', (req,res) => {
             console.log("User doesn't have an unique username");
             res.send("error")
         }
-    }) //find alle duration hvor man laver et null
+    })
 });
 
 app.post('/registerlesson/:lessonInfo', (req,res) => {
     console.log(req.session);
-
-    User.findOne(    {$and:[{_id:req.session.userId},{Usertype: "Admin"}]},(error,result)=>{ //Søger på users i databasen og bliver tildelt et unikt session id.
+    // Vi tjekker om req.session userid er en Admin, fordi en customer ikke må have adgang til
+    // at oprette en lesson
+    User.findOne({$and:[{_id:req.session.userId},{Usertype: "Admin"}]},(error,result)=>{
         if(result){
             console.log(result);
-            let lessonInfo = JSON.parse(req.params.lessonInfo);
-            console.log(lessonInfo);
-            Lesson.create(lessonInfo, (error2, result2) => {
+            console.log(JSON.parse(req.params.lessonInfo));
+            // Vi opretter en lesson. For at vi kan sende objektet lessonInfo gennem databasen, benytter vi JSON.parse
+            Lesson.create(JSON.parse(req.params.lessonInfo), (error2, result2) => {
                 if (result2) {
-
-                    console.log("worked");
+                    // JSON.stringify gør, at vi henter dataen tilbage
                     res.send(JSON.stringify(result2));
                 } else {
-                    console.log(error2);
+                    // Alle lektioner skal have et unikt lektionsnavn
                     console.log("Your lesson doesn't have an unique name");
                     res.send("error")
                 }
-            }) //find alle duration hvor man laver et null// Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
-        } else{
-            res.send("error2")
+            })
+        } else {
+            res.send("error2") // vi smider forskellige strings ind, for at detektere fejl hurtigere
         }
     })
-
 });
 
 app.post("/loginuser", (req,res) => {
-    //let userdata = JSON.parse(req.body);
-    console.log(req.body);
-
-    User.findOne({Username: req.body[0]}, (error, result) => {
-        if (result) {
+    console.log(req.body); //
+    User.findOne({Username: req.body[0]}, (error, result) => { //req.body er et array af 2 ting (username og password)
+        if (result){
+            //dekryptere for at se om det indtastede er den korrekte password
             bcrypt.compare(req.body[1], result.Password, (error, same) => {
                 if (same) {
                     console.log(result._id);
                     req.session.userId = result._id;
                     console.log(req.session);
                     console.log("User info confirmed");
-
+                    // henter usertype info tilbage fra databasen
                     res.send(JSON.stringify([req.body[0],result.Usertype]))
                 }
                 else{
@@ -137,84 +121,62 @@ app.post("/loginuser", (req,res) => {
     })
 });
 
-
-app.post('/AdminSite', async (req,res) => {
-    console.log(req.body);
-    await Lesson.create(req.body,(error,lesson) =>{
-        res.redirect('/AdminSite')
-    })
-});
-
-
-
 app.get("/userInfo", (req,res) =>{
     console.log(req.session);
-    User.findOne(    {$and:[{_id:req.session.userId},{Usertype: "Customer"}]},(error,result)=>{ //Søger på users i databasen og bliver tildelt et unikt session id.
-       if(result){
+    //Tjekker at det er den rigtige user, der får adgang til MyPageUser
+    User.findOne({$and:[{_id:req.session.userId},{Usertype: "Customer"}]},(error,result)=>{
+       if(result){ // hvis rigtigt bruger vi JSON.stringify for at afhente data
            console.log(result);
-               res.send(JSON.stringify(result)); // Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
-         } else{
+               res.send(JSON.stringify(result));
+         } else{ //hvis fejl, får vi udskrevet denne string
            res.send("No profiles found")
        }
    })
 });
 
-
-
 app.get('/lessonboard', (req,res)=> {
-    User.findOne(    {$and:[{_id:req.session.userId},{Usertype: "Admin"}]},(error,result)=>{ //Søger på users i databasen og bliver tildelt et unikt session id.
-        if(result){
+    User.findOne({$and:[{_id:req.session.userId},{Usertype: "Admin"}]},(error,result)=>{
+        if(result){ // hvis man er en admin får man lov til at se lektionerne.
             Lesson.find({Duration: {$gt: 0}}, (error, result) => {
                 if (result) {
                     res.send(JSON.stringify(result))
                 } else {
                     res.send("No profiles found")
                 }
-            }) //find alle duration hvor man laver et null// Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
+            }) //find alle duration hvor man laver et null. (det gør vi med alle).
         } else{
             res.send("error2")
         }
     })
-
-
-
-    //find alle duration hvor man laver et null (det gør vi med alle).
 });
-
 
 app.get('/lessonboard2', (req,res)=> {
-    User.findOne(    {$and:[{_id:req.session.userId},{Usertype: "Customer"}]},(error,result)=>{ //Søger på users i databasen og bliver tildelt et unikt session id.
-        if(result){
+    User.findOne({$and:[{_id:req.session.userId},{Usertype: "Customer"}]},(error,result)=>{ //Søger på users i databasen og bliver tildelt et unikt session id.
+        if(result){ // hvis man er en customer får man lov til at se lektionerne.
             Lesson.find({Duration: {$gt: 0}}, (error, result) => {
                 if (result) {
                     res.send(JSON.stringify(result))
                 } else {
                     res.send("No profiles found")
                 }
-            }) //find alle duration hvor man laver et null// Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
+            }) //find alle duration hvor man laver et null  (det gør vi med alle).
         } else{
             res.send("error2")
         }
     })
-
-
-
-    //find alle duration hvor man laver et null (det gør vi med alle).
 });
+
 app.get('/mylessonboard/:lesson', (req,res)=> {
     UserLesson.find({userid: req.session.userId}, (error,result) => {
-        if(result){
-            var temp = [];
+        if(result){ // returnere ale userlessons som brugeren har booket.
+            var temp = []; // her oprettes et array, hvor userlesson bliver tilføjet
             for(var i=0;i<result.length;i++){
                 if(result[i].lessonid){
-
                     temp.push(result[i].lessonid)
                 }
             }
-
-            Lesson.find({_id:    {$in:temp}}, (error1, result1) => {
-                if (result1) {
-
+            Lesson.find({_id:{$in:temp}}, (error1, result1) => {
+                if (result1) { // vi finder de Lessons som er i temp-arrayet. Altså de, der er booket af brugeren
                     res.send(JSON.stringify(result1))
                 } else {
                     res.send("No profiles found")
@@ -224,42 +186,37 @@ app.get('/mylessonboard/:lesson', (req,res)=> {
             res.send("Say Psych")
         }
     });
-    //find alle participant names hvor man laver et null
 });
-
-
-
-
 
 app.delete('/deletelesson/:lesson', (req,res)=> {
 console.log(req.params.lesson);
-    Lesson.deleteOne({Title: req.params.lesson}, (error, result) => {
+    Lesson.deleteOne({Title: req.params.lesson}, (error, result) => { // du sletter en lektion ud fra den informerede titel
         if (result) {
             res.send(JSON.stringify(result))
         } else {
             res.send("No profiles found")
         }
-    }) //find en lesson hvor man laver et null og sletter denne bestemte lesson.
+    })
 });
 
-
-app.put('/booklesson/:lesson', (req,res)=> { // :lesson accepterer en hver string værdi under objektet.
- Lesson.find({_id:JSON.parse(req.params.lesson)}, (error, result)=> {
+app.put('/booklesson/:lesson', (req,res)=> {
+ Lesson.find({_id:JSON.parse(req.params.lesson)}, (error, result)=> { // vi finder den lektion vi vil booke ud fra id'et
      if(result){
          UserLesson.find({lessonid:JSON.parse(req.params.lesson)}, (error1, result2)=> {
+             // tjekker hvor mange Userlessons, som har det unikke id til den lektion der ønskes at booke.
              if(result2){
-                 if(result2.length==result[0].Participants){
+                 if(result2.length==result[0].Participants){ // result[0] er lektionen.
                      console.log("The Lesson is fully booked");
                      res.send("error2")
                  } else{
-
                      UserLesson.findOne({userid:req.session.userId, lessonid:JSON.parse(req.params.lesson)},(error3,result3)=> {
+                         // Hvis der allerede er en UserLesson, som har præcis samme user:id, kan vi ikke booke denne lesson.
                          if (result3) {
-                             console.log("You cannot book the same Lesson twice");
+                             console.log("You cannot book the same Lesson twice"); // undgå dobbeltbooking
                              res.send("error")
                          } else {
                              UserLesson.create({userid:req.session.userId, lessonid:JSON.parse(req.params.lesson)},(error4,result4)=> {
-                                 if (result4) {
+                                 if (result4) { // opretter UserLesson hvis kriterierne er opfyldt.
                                      res.send(JSON.stringify(result4))
                                  } else {
                                      res.send("UserLesson couldnt be created")
@@ -276,33 +233,16 @@ app.put('/booklesson/:lesson', (req,res)=> { // :lesson accepterer en hver strin
          console.log("Error")
      }
  });
+});
 
-
-
-
-
-
-
-
-    //find alle duration hvor man laver et null
-})
-
-
-//Userlesson.find(userid: req.session.userid){
-//
-//}
 app.put('/cancellesson/:lesson', (req,res)=> {
-
 console.log(req.session);
-    let query = {$and:[{userid: req.session.userId},{lessonid: JSON.parse(req.params.lesson)[0]}]};
-    console.log(query);
-   console.log( JSON.stringify(query,null,2));
-    UserLesson.deleteOne(query, (error,result)=>{
-        if(result){
+    UserLesson.deleteOne({$and:[{userid: req.session.userId},{lessonid: JSON.parse(req.params.lesson)[0]}]}, (error,result)=>{
+        if(result){ // Sletter den user, hvor userlesson_id og user_id passer sammen. Fordi det er many-to-many forhold, må man tjekke begge veje.
+            // En user kan være tilmeldt mange lesson, og en lesson kan have mange tilmeldte.
             console.log(result);
             res.send(result);
         } else{
-
             console.log(error);
             res.send(result);
         }
@@ -310,77 +250,58 @@ console.log(req.session);
 });
 
 app.get("/changelessoninfo/:lesson", (req,res) =>{
-
-    Lesson.findOne({Title:req.params.lesson},(error,result)=>{
+    Lesson.findOne({Title:req.params.lesson},(error,result)=>{ //finder den lektion man gerne vil ændre ud fra titel og returnerer dataen.
         if(result) {
-
-
-            res.send(JSON.stringify(result))  // Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
+            res.send(JSON.stringify(result))
         }
         else{
             res.send("No profiles found")
         }
     })
-
 });
-
 
 app.put('/changelessoninfomation/:lesson', (req,res)=> {
-    let value = req.params.lesson;
-    var values = JSON.parse(value);
-console.log("virker det")
-    var myquery = {Title: values[0]};
-    console.log(myquery);
-    console.log(values[1]);
-    var newvalues = {$set: values[1]};
-    Lesson.updateOne(myquery,newvalues, (error, result) => {
+    console.log(JSON.parse(req.params.lesson)[1]);
+    Lesson.updateOne({Title: JSON.parse(req.params.lesson)[0]},{$set: JSON.parse(req.params.lesson)[1]}, (error, result) => {
+        // opdaterer informationerne ud fra titel-key.
         if (result) {
-            console.log("ggg");
+            console.log("det virker :)");
             if(error){
                 res.send("error")
             } else {
                 res.send(JSON.stringify(result));
             }
         } else {
-
             res.send("error")
         }
-    }) //find alle duration hvor man laver et null
+    })
 });
 
-
 app.get("/getuserinfo", (req,res) =>{
-
     User.findOne({_id:req.session.userId},(error,result)=>{
-        if(result) {
-            res.send(JSON.stringify(result))  // Tjekker userid og den user som er logget ind så man undgår at komme ind på andre brugeres side og se deres oplysninger.
+        if(result) { // før vi kan ændre på userinfo, skal vi hente dem.
+            res.send(JSON.stringify(result))
         }
         else{
             res.send("No profiles found")
         }
     })
-
 });
 
-
 app.put('/changeuserinfo/:userinfo', (req,res)=> {
-    User.updateOne(    {$and:[{_id:req.session.userId},{Usertype: "Customer"}]},{$set: JSON.parse(req.params.userinfo)}, (error, result) => {
+    User.updateOne({$and:[{_id:req.session.userId},{Usertype: "Customer"}]},{$set: JSON.parse(req.params.userinfo)}, (error, result) => {
+        // tjekker at du er en custumer, bagefter ændrer den dine informationer.
         if (result) {
-            console.log("ggg");
+            console.log("result");
             if(error){
                 res.send("error")
             } else {
                 res.send(JSON.stringify(result));
             }
         } else {
-
             res.send("error")
         }
-    }) //find alle duration hvor man laver et null
+    })
 });
 
-
 app.use((req,res) =>res.render('notfound'));
-
-
-
